4ce469ec5e34a986812634481b7b749ca0131fe9
tbpgr <tbpgr@tbpgr.jp>
2014-01-22 23:43:23 +0900

    fix rubocop warnings

--- lib/sublime_sunippetter.rb
+++ lib/sublime_sunippetter.rb
@@ -1,13 +1,14 @@
+# encoding: utf-8
 require 'sublime_sunippetter/version'
 require 'erb'
 
 module SublimeSunippetter
-  # = SublimeSunippetter Core
+  # SublimeSunippetter Core
   class Core
-    # == Sunippetdefine file name.
+    # Sunippetdefine file name.
     DEFINE_FILE = 'Sunippetdefine'
 
-    # == Sunippetdefine file template
+    # Sunippetdefine file template
     DEFINE_FILE_TEMPLATE = <<-EOS
 # encoding: utf-8
 
@@ -27,7 +28,7 @@ module SublimeSunippetter
 # add :hoge2, :args1, :args2, "block@b"
     EOS
 
-    # == sublime sunippet template
+    # sublime sunippet template
     SUNIPPET_TEMPLATE = <<-EOS
 <snippet>
   <content><![CDATA[
@@ -39,26 +40,33 @@ module SublimeSunippetter
 </snippet>
     EOS
 
-    # == generate Sunippetdefine to current directory.
+    # generate Sunippetdefine to current directory.
     def init
       File.open("./#{DEFINE_FILE}", 'w') { |f|f.puts DEFINE_FILE_TEMPLATE }
     end
 
-    # == generate sublime text2 sunippets from Sunippetdefine
+    # generate sublime text2 sunippets from Sunippetdefine
     def generate_sunippets
       sunippet_define = read_sunippetdefine
       dsl = Dsl.new
       dsl.instance_eval sunippet_define
       dsl.target_methods.each do |m|
-        snippet = get_snippet(m.method_name , get_args_names(m) , get_do_block(m).chop, get_brace_block(m), dsl._scope)
-        File.open("#{dsl._output_path}/#{m.method_name.to_s.tr('?', '')}.sublime-snippet", 'w:UTF-8') { |f|f.puts snippet }
+        snippet = get_snippet(
+          m.method_name ,
+          get_args_names(m) ,
+          get_do_block(m).chop,
+          get_brace_block(m),
+          dsl._scope
+        )
+        filename = "#{dsl._output_path}/#{m.method_name.to_s.tr('?', '')}.sublime-snippet"
+        File.open(filename, 'w:UTF-8') { |f|f.puts snippet }
       end
     end
 
     private
     def read_sunippetdefine
       unless File.exists? "./#{DEFINE_FILE}"
-        fail SunippetterError.new("you must create #{DEFINE_FILE}. manual or 'suni init' command")
+        fail SunippetterError, "you must create #{DEFINE_FILE}. manual or 'suni init' command"
       end
       File.read("./#{DEFINE_FILE}")
     end
@@ -73,7 +81,7 @@ module SublimeSunippetter
       args = _method.args
       args_names = ' '
       args.each_with_index do |a, i|
-         args_names << "${#{i + 1}:#{a}}, "
+        args_names << "${#{i + 1}:#{a}}, "
       end
       args_names.chop!.chop! unless args.empty?
     end
@@ -93,11 +101,11 @@ end
     end
   end
 
-  # = TargetMethod. this is method information container
+  # TargetMethod. this is method information container
   class TargetMethod
     attr_accessor :method_name, :args, :has_do_block, :has_brace_block
 
-    # == generate sublime text2 sunippets from Sunippetdefine
+    # generate sublime text2 sunippets from Sunippetdefine
     def initialize(&block)
       instance_eval do
         block.call(self)
@@ -105,18 +113,18 @@ end
     end
   end
 
-  # = Dsl. this is dsl for Sunippetdefine.
+  # Dsl. this is dsl for Sunippetdefine.
   class Dsl
     attr_accessor :target_methods, :_scope, :_output_path
 
-    # == init default values
+    # init default values
     def initialize
       @target_methods = []
       @_scope = 'source.ruby'
       @_output_path = './'
     end
 
-    # == add sunippet information
+    # add sunippet information
     def add(method_name, *args)
       return if method_name.nil?
       return if method_name.empty?
@@ -133,14 +141,14 @@ end
       end
     end
 
-    # == set sunippet scope
+    # set sunippet scope
     def scope(_scope)
       return if _scope.nil?
       return if _scope.empty?
       @_scope = _scope
     end
 
-    # == set sunippet output path
+    # set sunippet output path
     def output_path(_output_path)
       return if _output_path.nil?
       return if _output_path.empty?
